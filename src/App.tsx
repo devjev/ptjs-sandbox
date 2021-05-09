e08157627f695d382fd752a588ddeb50d9dd9993
Jevgeni Tarasov <jevgeni@hey.com>
2021-05-09 16:01:12 +0200

    Messing about

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { PtsCanvas } from 'react-pts-canvas'
+import './App.css'
+import { Num, Bound, CanvasSpace, Group, Rectangle } from 'pts'
+
+class AppCanvas extends PtsCanvas {
+  animate(time: number, ftime: number) {
+    const rect = Rectangle.fromCenter(this.space.center, this.space.size.$divide(2))
+    const poly = Rectangle.corners(rect)
+    poly.shear2D( Num.cycle(time / 10000) - 0.5, this.space.center)
+
+    this.form.fillOnly("#123").polygon(poly)
+
+    const radius = 10 + Num.cycle(time / 5000) * 100
+    this.form.fill("rgb(0, 71, 187)").point(this.space.pointer, radius, "circle")
+
+    // triangles
+    const triangles = poly.segments(2, 1, true)
+    triangles.forEach((triangle) => {
+      triangle.push(this.space.pointer)
+    })
+
+    this.form.strokeOnly("#fff", 2).polygons(triangles)
+  }
+
+  start(bound: Bound, space: CanvasSpace) {
+  }
+
+  action(type: string, x: number, y: number, event: Event) {}
+
+  resize(size: Group, event: Event) {}
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       <AppCanvas
+         style={{height: "100vh"}}
+         background={"rgb(217, 217, 214)"} />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
